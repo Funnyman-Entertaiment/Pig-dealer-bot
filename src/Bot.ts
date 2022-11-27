ab4bdd7ebcb54e2e27902ac5df1a92d369c30561
thicco-catto <cristobal.martin01@gmail.com>
2022-11-27 09:15:04 +0100

    lol lmao

--- src/Bot.ts
+++ src/Bot.ts
@@ -5,6 +5,20 @@ import * as DotEnv from "dotenv";
 import ready from "./listeners/Ready";
 import interactionCreate from "./listeners/InteractionCreate";
 import { PackDropper } from "./events/PackDropper";
+import * as http from "http";
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
 
 DotEnv.config();
 
