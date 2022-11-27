849279bfceb4b27482335e3ff9e96ab303e8224d
thicco-catto <cristobal.martin01@gmail.com>
2022-11-27 16:13:15 +0100

    tweak chances

--- dist/Constants/RaritiesPerPigCount.js
+++ dist/Constants/RaritiesPerPigCount.js
@@ -24,7 +24,7 @@ exports.RARITIES_PER_PIG_COUNT = {
         { "Common": 1 },
         { "Common": 1 },
         { "Common": 1, "Rare": 0.25 },
-        { "Common": 1, "Rare": 0.5, "Epic": 0.1 },
+        { "Common": 1, "Rare": 0.25 },
         { "Rare": 1, "Epic": 0.1 },
         { "Rare": 1, "Epic": 0.1, "Legendary": 0.1 }
     ],
--- src/Constants/RaritiesPerPigCount.ts
+++ src/Constants/RaritiesPerPigCount.ts
@@ -21,7 +21,7 @@ export const RARITIES_PER_PIG_COUNT: { readonly [key: number]: {readonly [key: s
         {"Common": 1},
         {"Common": 1},
         {"Common": 1, "Rare": 0.25},
-        {"Common": 1, "Rare": 0.5, "Epic": 0.1},
+        {"Common": 1, "Rare": 0.25},
         {"Rare": 1, "Epic": 0.1},
         {"Rare": 1, "Epic": 0.1, "Legendary": 0.1}
     ],
