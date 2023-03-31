c34af0c0e5bda4df14bf70fc6662690556461481
thicco-catto <cristobal.martin01@gmail.com>
2023-04-01 00:12:56 +0200

    Changed server and user reset to use batch update

--- dist/events/ResetServerAndUserInfo.js
+++ dist/events/ResetServerAndUserInfo.js
@@ -3,37 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ResetServerAndUserInfo = void 0;
 const lite_1 = require("firebase/firestore/lite");
 const Bot_1 = require("../Bot");
-const ServerInfo_1 = require("../database/ServerInfo");
-const UserInfo_1 = require("../database/UserInfo");
 const Log_1 = require("../Utils/Log");
 async function ResetServerUsers() {
     (0, Log_1.LogInfo)("Resetting server and user information.");
+    const batch = (0, lite_1.writeBatch)(Bot_1.db);
     const serverQuery = (0, lite_1.query)((0, lite_1.collection)(Bot_1.db, "serverInfo"));
     const servers = await (0, lite_1.getDocs)(serverQuery);
     for (let i = 0; i < servers.size; i++) {
         const element = servers.docs[i];
-        const serverInfo = await (0, ServerInfo_1.GetServerInfo)(element.id);
-        console.log(`Server: ${i + 1}/${servers.size} (${element.id})`);
-        await new Promise(r => setTimeout(r, 200));
-        if (serverInfo !== undefined) {
-            serverInfo.Enabled = true;
-            await (0, ServerInfo_1.AddServerInfoToCache)(serverInfo);
-        }
+        batch.update(element.ref, {
+            Enabled: true
+        });
     }
-    (0, ServerInfo_1.SaveAllServerInfo)();
     const usersQuery = (0, lite_1.query)((0, lite_1.collection)(Bot_1.db, "users"));
     const users = await (0, lite_1.getDocs)(usersQuery);
     for (let i = 0; i < users.size; i++) {
         const element = users.docs[i];
-        const userInfo = await (0, UserInfo_1.GetUserInfo)(element.id);
-        console.log(`User: ${i + 1}/${users.size} (${element.id})`);
-        await new Promise(r => setTimeout(r, 200));
-        if (userInfo !== undefined) {
-            userInfo.WarnedAboutCooldown = false;
-            await (0, UserInfo_1.AddUserInfoToCache)(userInfo);
-        }
+        batch.update(element.ref, {
+            WarnedAboutCooldown: true
+        });
     }
-    (0, UserInfo_1.SaveAllUserInfo)();
+    await batch.commit();
 }
 async function ResetServerAndUserInfo() {
     await ResetServerUsers();
--- src/events/ResetServerAndUserInfo.ts
+++ src/events/ResetServerAndUserInfo.ts
@@ -1,4 +1,4 @@
-import { query, collection, getDocs } from "firebase/firestore/lite";
+import { query, collection, getDocs, WriteBatch, Firestore, writeBatch } from "firebase/firestore/lite";
 import { db } from "../Bot";
 import { AddServerInfoToCache, GetServerInfo, SaveAllServerInfo } from "../database/ServerInfo";
 import { AddUserInfoToCache, GetUserInfo, SaveAllUserInfo } from "../database/UserInfo";
@@ -7,39 +7,29 @@ import { LogInfo } from "../Utils/Log";
 async function ResetServerUsers(){
     LogInfo("Resetting server and user information.")
 
+    const batch = writeBatch(db);
+
     const serverQuery = query(collection(db, "serverInfo"));
     const servers = await getDocs(serverQuery);
 
     for (let i = 0; i < servers.size; i++) {
         const element = servers.docs[i];
-
-        const serverInfo = await GetServerInfo(element.id);
-        console.log(`Server: ${i+1}/${servers.size} (${element.id})`);
-        await new Promise(r => setTimeout(r, 200));
-        if(serverInfo !== undefined){
-            serverInfo.Enabled = true;
-            await AddServerInfoToCache(serverInfo);
-        }
+        batch.update(element.ref, {
+            Enabled: true
+        });
     }
-
-    SaveAllServerInfo();
 
     const usersQuery = query(collection(db, "users"));
     const users = await getDocs(usersQuery);
 
     for (let i = 0; i < users.size; i++) {
         const element = users.docs[i];
-        
-        const userInfo = await GetUserInfo(element.id);
-        console.log(`User: ${i+1}/${users.size} (${element.id})`);
-        await new Promise(r => setTimeout(r, 200));
-        if(userInfo !== undefined){
-            userInfo.WarnedAboutCooldown = false;
-            await AddUserInfoToCache(userInfo);
-        }
+        batch.update(element.ref, {
+            WarnedAboutCooldown: true
+        });
     }
 
-    SaveAllUserInfo();
+    await batch.commit();
 }
 
 export async function ResetServerAndUserInfo() {
