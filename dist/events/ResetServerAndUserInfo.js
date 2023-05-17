332b2417de309578291f148f5b521c3fd424acd2
thicco-catto <cristobal.martin01@gmail.com>
2023-05-17 12:19:09 +0200

    Compiled code

--- dist/events/ResetServerAndUserInfo.js
+++ dist/events/ResetServerAndUserInfo.js
@@ -6,24 +6,46 @@ const Bot_1 = require("../Bot");
 const Log_1 = require("../Utils/Log");
 async function ResetServerUsers() {
     (0, Log_1.LogInfo)("Resetting server and user information.");
-    const batch = (0, lite_1.writeBatch)(Bot_1.db);
+    const serverGroups = [];
+    serverGroups.push([]);
     const serverQuery = (0, lite_1.query)((0, lite_1.collection)(Bot_1.db, "serverInfo"));
     const servers = await (0, lite_1.getDocs)(serverQuery);
-    for (let i = 0; i < servers.size; i++) {
-        const element = servers.docs[i];
-        batch.update(element.ref, {
-            Enabled: true
+    servers.forEach(server => {
+        if (serverGroups[serverGroups.length - 1].length >= 400) {
+            serverGroups.push([]);
+        }
+        serverGroups[serverGroups.length - 1].push(server);
+    });
+    for (let i = 0; i < serverGroups.length; i++) {
+        const batch = (0, lite_1.writeBatch)(Bot_1.db);
+        const serverDocs = serverGroups[i];
+        serverDocs.forEach(element => {
+            batch.update(element.ref, {
+                Enabled: true
+            });
         });
+        await batch.commit();
     }
+    const userGroups = [];
+    userGroups.push([]);
     const usersQuery = (0, lite_1.query)((0, lite_1.collection)(Bot_1.db, "users"));
     const users = await (0, lite_1.getDocs)(usersQuery);
-    for (let i = 0; i < users.size; i++) {
-        const element = users.docs[i];
-        batch.update(element.ref, {
-            WarnedAboutCooldown: true
+    users.forEach(server => {
+        if (userGroups[userGroups.length - 1].length >= 400) {
+            userGroups.push([]);
+        }
+        userGroups[userGroups.length - 1].push(server);
+    });
+    for (let i = 0; i < userGroups.length; i++) {
+        const batch = (0, lite_1.writeBatch)(Bot_1.db);
+        const userDocs = userGroups[i];
+        userDocs.forEach(element => {
+            batch.update(element.ref, {
+                WarnedAboutCooldown: false
+            });
         });
+        await batch.commit();
     }
-    await batch.commit();
 }
 async function ResetServerAndUserInfo() {
     await ResetServerUsers();
